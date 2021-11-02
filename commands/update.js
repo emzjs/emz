75cd5b94cca7b7da607c007d15e71a03b9f69337
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-03 00:05:10 +0800

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 const chalk = require('chalk');
 const axios = require('axios');
 const update = async (option1, option2, runCommand) => {
-    if(fs.existsSync(`./emz.json`)){
+const file = fs.existsSync(`./emz.json`)
+    if(file){
+       if(!option1){
+        file?.packages?.forEach((e, i) => {
+          if(!file.packages[i].version) return console.log(chalk`
+            {red The Version Of ${file.packages[i]["package_name"] ? file.packages[i]["package_name"] : 'Unknown' } Was Undefined}
+          `);
+        })
+       }else {
        
+      }
     }else {
         console.log(chalk`
            {bold Couldn't find emz config file} - {grey.bold create one: emz init}
